b549e23667a012dd1decc8c958d02f6848f03543
AldrichAvilaS <aavilas1801@alumno.ipn.mx>
2023-05-09 23:51:43 -0600

    Update star.py

--- laberinto/Estrella/code/star.py
+++ laberinto/Estrella/code/star.py
@@ -2,9 +2,9 @@ import heapq
 import maze
 
 # Definir los movimientos permitidos (arriba, abajo, izquierda, derecha y diagonales)
-MOVIMIENTOS = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+MOVIMIENTOS = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
 
-def calcular_costo(punto_actual, punto_final, costo_terreno):
+def calcular_costo(punto_actual, punto_final, cost):
     """
     Calcula el costo heurístico entre dos puntos.
     Utilizamos la distancia Manhattan como heurística.
@@ -13,10 +13,11 @@ def calcular_costo(punto_actual, punto_final, costo_terreno):
     x2, y2 = punto_final
     return abs(x1 - x2) + abs(y1 - y2)
 
-def encontrar_ruta(mapa, punto_inicial, punto_final):
+def encontrar_ruta(mapa, punto_inicial, punto_final, cost):
     """
     Encuentra la ruta con el menor costo utilizando el algoritmo A*.
-    El mapa es una matriz bidimensional con los tipos de terreno y sus costos.
+    El mapa es una lista que indica el tipo de agente en cada casilla.
+    La lista cost representa los costos de movimiento.
     """
     filas = len(mapa)
     columnas = len(mapa[0])
@@ -37,10 +38,10 @@ def encontrar_ruta(mapa, punto_inicial, punto_final):
     # Crear una cola de prioridad para almacenar los puntos a explorar
     cola_prioridad = []
     heapq.heappush(cola_prioridad, (0, punto_inicial))
-
+    
     while cola_prioridad:
-        _, punto_actual = heapq.heappop(cola_prioridad)
-
+        punto_actual = heapq.heappop(cola_prioridad)[1]
+    
         if punto_actual == punto_final:
             # Se encontró el punto final, reconstruir la ruta y retornarla
             ruta = []
@@ -49,24 +50,23 @@ def encontrar_ruta(mapa, punto_inicial, punto_final):
                 punto_actual = ruta_anterior[punto_actual[0]][punto_actual[1]]
             ruta.reverse()
             return ruta
-
+    
         for movimiento in MOVIMIENTOS:
             dx, dy = movimiento
             nuevo_x = punto_actual[0] + dx
             nuevo_y = punto_actual[1] + dy
-
+    
             # Verificar si el nuevo punto está dentro de los límites del mapa
             if nuevo_x < 0 or nuevo_x >= filas or nuevo_y < 0 or nuevo_y >= columnas:
                 continue
-
-            # Calcular el costo acumulado para el nuevo punto
-            costo_nuevo = costo_acumulado[punto_actual[0]][punto_actual[1]] + costo_terreno[nuevo_x][nuevo_y]
-        # Verificar si el costo acumulado es menor al costo acumulado previo para el nuevo punto
-        if costo_nuevo < costo_acumulado[nuevo_x][nuevo_y]:
-            costo_acumulado[nuevo_x][nuevo_y] = costo_nuevo
-            costo_total = costo_nuevo + calcular_costo((nuevo_x, nuevo_y), punto_final, costo_terreno[nuevo_x][nuevo_y])
-            heapq.heappush(cola_prioridad, (costo_total, (nuevo_x, nuevo_y)))
-            ruta_anterior[nuevo_x][nuevo_y] = punto_actual
-
-# No se pudo encontrar una ruta válida
-raise ValueError("No se pudo encontrar una ruta válida.")
+            
+            tipo_agente = mapa[nuevo_x][nuevo_y]
+            costo = cost[tipo_agente][movimiento]
+    
+            # Verificar si el costo asociado al nuevo tipo de agente y movimiento es válido (diferente de 0)
+            # y si el costo acumulado es menor al costo acumulado previo para el nuevo punto
+            if costo != 0 and costo_acumulado[punto_actual[0]][punto_actual[1]] + costo < costo_acumulado[nuevo_x][nuevo_y]:
+                costo_acumulado[nuevo_x][nuevo_y] = costo_acumulado[punto_actual[0]][punto_actual[1]] + costo
+                costo_total = costo_acumulado[nuevo_x][nuevo_y] + calcular_costo((nuevo_x, nuevo_y), punto_final, cost)
+                heapq.heappush(cola_prioridad, (costo_total, (nuevo_x, nuevo_y)))
+                ruta_anterior[nuevo_x][nuevo_y] = punto_actual
